43718546ae0f7fbf740fc4fe6f072a988c12ed15
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2025-04-28 13:29:52 -0300

    Remover arquivos de cadastro de recrutas e calculadora, e adicionar conversor de medidas

--- Exercicio_02/calculoDano.js
+++ Exercicio_02/calculoDano.js
@@ -1,14 +1,14 @@
 const nomePersonagem1 = prompt(
-  "Digite o nome do personagem que realizará o ataque: "
+  "Digite o nome do personagem que realizará o ataque: ",
 );
 let poderAtaque = Number(
-  prompt(`Digite o poder de ataque do personagem ${nomePersonagem1}:`)
+  prompt(`Digite o poder de ataque do personagem ${nomePersonagem1}:`),
 );
 
 const nomePersonagem2 = prompt("Digite o nome do segundo personagem: ");
 let vidaPersonagem = Number(prompt("Digite a vida do segundo personagem: "));
 let defesaPersonagem = Number(
-  prompt("Digite a defesa do segundo personagem: ")
+  prompt("Digite a defesa do segundo personagem: "),
 );
 let escudo =
   prompt("O segundo personagem tem escudo? (sim/não):").toLowerCase() === "sim";
@@ -26,5 +26,5 @@ let vidaFinal = vidaPersonagem - dano;
 console.log(
   `O personagem ${nomePersonagem1} causou ${dano} de dano no personagem ${nomePersonagem2}, que tinha ${vidaPersonagem} de vida e agora tem ${vidaFinal} de vida restante.${
     escudo ? " O personagem estava com escudo." : ""
-  }`
+  }`,
 );
---
+++ Exercicio_03/conversorDeMedidas.js
@@ -0,0 +1,57 @@
+// Conversor de Medidas
+
+// Escreva um programa em javascript que funcione como um conversos de medidas. O programa deverá pedir por um valor em metros e então dar a opção de escolher para qual unidade de medida esse valor deve ser convertido. As opções são:
+
+// milímetro (mm)
+// centímetro (cm)
+// decímetro (dm)
+// decâmetro (dam)
+// hectômetro (hm)
+// quilômetro (km)
+
+// O programa deve então converter a medida de acordo com a opção escolhida e exibir o resultado.
+
+// O programa também deve exibir uma mensagem de “Opção inválida” caso o usuário insira uma opção diferente das disponíveis (use o break e o default para isso)
+
+let valorEmMetros = parseFloat(prompt("Digite o valor em metros:"));
+let unidade = parseFloat(
+  prompt(
+    "Escolha a unidade para conversão:\n1. milímetro (mm)\n2. centímetro (cm)\n3. decímetro (dm)\n4. decâmetro (dam)\n5. hectômetro (hm)\n6. quilômetro (km)\nDigite o número correspondente à unidade desejada:"
+  )
+);
+
+switch (unidade) {
+  case 1:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros * 1000} milímetros.`
+    );
+    break;
+  case 2:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros * 100} centímetros.`
+    );
+    break;
+  case 3:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros * 10} decímetros.`
+    );
+    break;
+  case 4:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros / 10} decâmetros.`
+    );
+    break;
+  case 5:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros / 100} hectômetros.`
+    );
+    break;
+  case 6:
+    console.log(
+      `${valorEmMetros} metros é igual a ${valorEmMetros / 1000} quilômetros.`
+    );
+    break;
+  default:
+    console.log("Opção inválida. Por favor, escolha uma unidade válida.");
+    break;
+}
