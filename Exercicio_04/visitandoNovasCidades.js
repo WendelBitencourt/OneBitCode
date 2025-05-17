7c6f7c32d51afbbd42be30ac04c70c45d4e45352
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2025-05-17 00:03:02 -0300

    Ajustar prompts e estrutura do programa para registrar cidades visitadas por turistas

--- Exercicio_04/visitandoNovasCidades.js
+++ Exercicio_04/visitandoNovasCidades.js
@@ -6,9 +6,21 @@
 // e então continue perguntando se o turista visitou alguma outra cidade até que a resposta seja não.
 // No fim, o programa deve exibir o nome do turista, quantas e quais cidades ele visitou.
 
-let nomeTurista = prompt("Qual é o seu nome?");
+let nomeTurista = prompt("Qual é o seu nome?: ");
+let visitouCidade = prompt("Você já visitou alguma cidade?: ");
+let nomeCidade = "";
+let cidades = "";
+let contagemCidade = 0;
 
-let nomeCidade;
-let visitouCidade;
+while (visitouCidade === "Sim" || visitouCidade === "sim") {
+  nomeCidade = prompt("Digite o nome da cidade visitada: ");
+  cidades += ` - ${nomeCidade} \n`;
+  contagemCidade++;
+  visitouCidade = prompt("Você já visitou alguma cidade?: ");
+}
 
-while (visitouCidade === "Sim" || visitouCidade === "sim") {}
+console.log(
+  `Turista: ${nomeTurista}
+  \nQuantidade de cidades: ${contagemCidade}
+  \nCidades Visitadas:\n${cidades} `
+);
