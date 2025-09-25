3defb1fa143839439535a244dc4514a9c1f13ced
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2025-09-25 10:10:18 -0300

    Capitalize output and normalize input in palindrome check

--- Exercicio_06/palindromo.js
+++ Exercicio_06/palindromo.js
@@ -2,7 +2,27 @@
 
 // Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.
 
-let palavra = prompt("Digite uma palavra: ");
+function capitalizarPrimeiraLetra(word) {
+  // Garante que a entrada é uma string e não está vazia
+  if (typeof word !== "string" || word.length === 0) {
+    return "";
+  }
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function capitalizarPalavra(string) {
+  // Garante que a entrada é uma string e não está vazia
+  if (typeof string !== "string" || string.length === 0) {
+    return "";
+  }
+
+  return string
+    .split(" ")
+    .map((word) => capitalizarPrimeiraLetra(word))
+    .join(" ");
+}
+
+let palavra = prompt("Digite uma palavra: ").toLowerCase().trim();
 let palavraInvertida = "";
 
 for (let i = palavra.length - 1; i >= 0; i--) {
@@ -10,9 +30,13 @@ for (let i = palavra.length - 1; i >= 0; i--) {
 }
 
 if (palavra === palavraInvertida) {
-  console.log(`A palavra ${palavra} é um palíndromo.`);
+  console.log(`A palavra ${capitalizarPalavra(palavra)} é um palíndromo.`);
 } else {
   console.log(
-    `A palavra ${palavra} não é um palíndromo. Ela lida de trás para frente é ${palavraInvertida}.`,
+    `A palavra ${capitalizarPalavra(
+      palavra,
+    )} não é um palíndromo. Ela lida de trás para frente é ${capitalizarPalavra(
+      palavraInvertida,
+    )}.`,
   );
 }
